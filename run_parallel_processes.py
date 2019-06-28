de6cda5c95747b43b762dae634285822fddb605f
ChaseD13 <chasedudas13@gmail.com>
2019-06-28 10:31:29 -0600

    Added file to process queue

--- run_parallel_processes.py
+++ run_parallel_processes.py
@@ -1,13 +1,13 @@
 import os                                                                       
-from multiprocessing import Pool                                                
+from multiprocessing import Pool, Queue                                               
                                                                                 
                                                                                 
-processes = ('UI-lab-capture.py')##, 'process2.py', 'process3.py')                                    
+processes = ('UI-lab-capture.py', 'acquire_images.py', 'run_parallel_processes.py')                                    
                                                   
                                                                                 
 def run_process(process):                                                             
     os.system('conda run {}'.format(process))                                       
                                                                                 
-                                                                                
-pool = Pool(processes=1)                                                        
+shared_queue = Queue()                                                              
+pool = Pool(processes=3)                                                        
 pool.map(run_process, processes)   
