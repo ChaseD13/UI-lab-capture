fb94c7e4c69615ad540139eca71c0c4e7953622d
ChaseD13 <chasedudas13@gmail.com>
2019-06-28 11:32:46 -0600

    Add implementation for aquire and append scripts

--- acquire_images.py
+++ acquire_images.py
@@ -1,1 +1,13 @@
 # This script will init and collect images from both the primary and secondary cameras
+import sys
+import PySpin
+
+camera_list = [sys.argv[1], sys.argv[2]]
+queue = [sys.argv[3], sys.argv[4]]
+
+for i in range(2):
+    image = camera_list[i].GetNextImage()
+    queue[i].put(image)
+    # Display image to preview
+
+
--- append_images.py
+++ append_images.py
@@ -1,1 +1,10 @@
 # This script will append collected images to MPJG video
+import sys
+import PySpin
+
+queue = [sys.argv[1], sys.argv[2]]
+video = [sys.argv[3], sys.argv[4]]
+
+for i in range(2):
+    image = queue[i].get()
+    video[i].Append(image)
